6729b7e5dbf0be810178592d4d420fa4e998ad53
rlaplaza <laplazasolanas@gmail.com>
2021-12-20 20:42:01 +0100

    Bugfix

--- run_DRL.py
+++ run_DRL.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import numpy as np
 import time
-from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 # preprocessor
 from preprocessing.preprocessors import *

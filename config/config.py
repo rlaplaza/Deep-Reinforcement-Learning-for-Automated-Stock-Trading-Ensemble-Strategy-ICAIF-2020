14cd8e7b1d6cc828a1daedee881dc24cc393eb5d
rlaplaza <laplazasolanas@gmail.com>
2021-12-20 22:49:31 +0100

    Continued reformatting and updating for stable_baselines3

--- config/config.py
+++ config/config.py
@@ -1,22 +1,23 @@
 import pathlib
 
-#import finrl
+# import finrl
 
 import pandas as pd
 import datetime
 import os
-#pd.options.display.max_rows = 10
-#pd.options.display.max_columns = 10
+
+# pd.options.display.max_rows = 10
+# pd.options.display.max_columns = 10
 
 
-#PACKAGE_ROOT = pathlib.Path(finrl.__file__).resolve().parent
-#PACKAGE_ROOT = pathlib.Path().resolve().parent
+# PACKAGE_ROOT = pathlib.Path(finrl.__file__).resolve().parent
+# PACKAGE_ROOT = pathlib.Path().resolve().parent
 
-#TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
-#DATASET_DIR = PACKAGE_ROOT / "data"
+# TRAINED_MODEL_DIR = PACKAGE_ROOT / "trained_models"
+# DATASET_DIR = PACKAGE_ROOT / "data"
 
 # data
-#TRAINING_DATA_FILE = "data/ETF_SPY_2009_2020.csv"
+# TRAINING_DATA_FILE = "data/ETF_SPY_2009_2020.csv"
 TRAINING_DATA_FILE = "data/dow_30_2009_2020.csv"
 
 now = datetime.datetime.now()
@@ -25,5 +26,3 @@ os.makedirs(TRAINED_MODEL_DIR)
 TURBULENCE_DATA = "data/dow30_turbulence_index.csv"
 
 TESTING_DATA_FILE = "test.csv"
-
-
--- run_DRL.py
+++ run_DRL.py
@@ -6,11 +6,14 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 
 # preprocessor
 from preprocessing.preprocessors import *
+
 # config
 from config.config import *
+
 # model
 from model.models import *
 import os
+
 
 def run_model() -> None:
     """Train the model."""
@@ -30,21 +33,26 @@ def run_model() -> None:
     # 2015/10/01 is the date that validation starts
     # 2016/01/01 is the date that real trading starts
     # unique_trade_date needs to start from 2015/10/01 for validation purpose
-    unique_trade_date = data[(data.datadate > 20151001)&(data.datadate <= 20200707)].datadate.unique()
+    unique_trade_date = data[
+        (data.datadate > 20151001) & (data.datadate <= 20200707)
+    ].datadate.unique()
     print(unique_trade_date)
 
     # rebalance_window is the number of months to retrain the model
     # validation_window is the number of months to validation the model and select for trading
     rebalance_window = 63
     validation_window = 63
-    
-    ## Ensemble Strategy
-    run_ensemble_strategy(df=data, 
-                          unique_trade_date= unique_trade_date,
-                          rebalance_window = rebalance_window,
-                          validation_window=validation_window)
 
-    #_logger.info(f"saving model version: {_version}")
+    ## Ensemble Strategy
+    run_ensemble_strategy(
+        df=data,
+        unique_trade_date=unique_trade_date,
+        rebalance_window=rebalance_window,
+        validation_window=validation_window,
+    )
+
+    # _logger.info(f"saving model version: {_version}")
+
 
 if __name__ == "__main__":
     run_model()
